f873b7274bc8453ad5e92a74c27ac7d493e6662c
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-05-24 23:38:15 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import styles from "./App.module.scss";
 import Form from "./components/Form";
-import { useState } from "react";
 import RecordList from "./components/RecordList";
 import Filter from "./components/Filter";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
+import styles from "./App.module.scss";
+
 function App() {
-  const [filter, setFilter] = useState("");
-
-  // const deleteRecord = (name) => {
-  //   const localData = JSON.parse(localStorage.getItem("records"));
-  //   const newLocalRecords = localData.filter((obj) => obj.name !== name);
-  //   localStorage.setItem("records", JSON.stringify(newLocalRecords));
-  // };
-
-  // const getData = (data) => {
-  //   setData((prev) => prev.concat([data]));
-  // };
-
-  // const getFilterValue = (filter) => {
-  //   setFilter(filter);
-  // };
-
   return (
     <Provider store={store}>
       <div className={styles.App}>
         <h1 className={styles.App__title}>AddressBook</h1>
         <main className={styles.App__mainWrapper}>
-          <Form/>
+          <Form />
           <div className={styles.App__RecordsWrapper}>
-            <Filter/>
-            <RecordList
-              filter={filter}
-            />
+            <Filter />
+            <RecordList />
           </div>
         </main>
       </div>
